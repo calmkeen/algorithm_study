68d80809e2442967871e94217e7400192171e5ce
calmkeen <iyunseong159@gmail.com>
2021-12-14 17:01:16 +0900

    1330번문제

---
+++ backjoon/baekjoon1330_C.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int A, B;
+
+	scanf("%d %d", &A, &B);
+
+	if (A > B)
+	{
+		printf(">");
+		
+	}
+	else if (A < B)
+	{
+		printf("<");
+	}
+	else if (A = B)
+	{
+		printf("==");
+	}
+	
+	
+	
+
+	
+	return 0;
+}
