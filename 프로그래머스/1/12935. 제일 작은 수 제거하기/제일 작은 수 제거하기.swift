bba7fc2f8507387928f8b6b4e9d664cba7c0a759
Younsung <78361650+calmkeen@users.noreply.github.com>
2024-08-29 12:53:17 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 9.42 ms, Memory: 25.3 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.swift
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.swift
@@ -1,6 +1,3 @@
-func solution(_ arr: [Int]) -> [Int] {
-   let minValue = arr.min()!
-    
-    var solve = arr.filter { $0 != minValue }
-    return solve.isEmpty ? [-1] : solve
+func solution(_ arr:[Int]) -> [Int] {
+    return []
 }
