7694b43750efbcdb77778a947c750ed335348361
Younsung <78361650+calmkeen@users.noreply.github.com>
2025-10-25 15:34:02 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    var result = 0
+    
+    for i in 1...n {
+        if i%2 != 0 {
+            continue
+        } else {
+            result += i
+        }
+    }
+    return result
+}
